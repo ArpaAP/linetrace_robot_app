f71abfd6ef56fdeafd5b46cfa08eb4ed404e23ce
ArpaAP <arpa1705@gmail.com>
2021-11-13 17:21:08 +0900

    Add: UI 개선

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:linetrace_robot_app/widgets/drawer.dart';
+import 'package:flutter/services.dart';
+import 'package:linetrace_robot_app/pages/hardware.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +21,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum ControlMode { arrow, gyro }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -32,187 +35,292 @@ class _HomePageState extends State<HomePage> {
   bool _loopActive = false;
   bool _activeAutopilot = false;
 
+  ControlMode _controlMode = ControlMode.arrow;
+  final _pageController = PageController();
+  bool isRotated = false;
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   void _increaseCounterWhilePressed() async {
-    // make sure that only one loop is active
     if (_loopActive) return;
 
     _loopActive = true;
-
     while (_buttonPressed) {
-      // do your thing
       print(DateTime.now());
-
-      // wait a bit
       await Future.delayed(Duration(milliseconds: 200));
     }
-
     _loopActive = false;
+  }
+
+  Widget autoModeControlWidget(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+
+  Widget manualModeControlWidget(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: [
+            RadioListTile(
+              value: ControlMode.arrow,
+              dense: true,
+              groupValue: _controlMode,
+              onChanged: (ControlMode? value) {
+                setState(() {
+                  _controlMode = value!;
+                });
+              },
+              title: Text('화살표 키'),
+              subtitle: Text('화면에 표시되는 좌우 화살표 키를 사용합니다.'),
+            ),
+            Divider(height: 0),
+            RadioListTile(
+              value: ControlMode.gyro,
+              dense: true,
+              groupValue: _controlMode,
+              onChanged: (ControlMode? value) {
+                setState(() {
+                  _controlMode = value!;
+                });
+              },
+              title: Text('자이로센서 사용'),
+              subtitle: Text('디바이스의 기울기에 따라 방향을 조정합니다.'),
+            )
+          ],
+        ),
+        Divider(height: 0),
+        SizedBox(height: 25),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Listener(
+              onPointerDown: (details) {
+                _buttonPressed = true;
+                _increaseCounterWhilePressed();
+              },
+              onPointerUp: (details) {
+                _buttonPressed = false;
+              },
+              child: SizedBox(
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      RotationTransition(
+                        turns: AlwaysStoppedAnimation(90 / 360),
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 36,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 12),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Listener(
+              onPointerDown: (details) {
+                _buttonPressed = true;
+                _increaseCounterWhilePressed();
+              },
+              onPointerUp: (details) {
+                _buttonPressed = false;
+              },
+              child: SizedBox(
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_back,
+                        size: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 12),
+            Listener(
+              onPointerDown: (details) {
+                _buttonPressed = true;
+                _increaseCounterWhilePressed();
+              },
+              onPointerUp: (details) {
+                _buttonPressed = false;
+              },
+              child: SizedBox(
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      RotationTransition(
+                        turns: AlwaysStoppedAnimation(90 / 360),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          size: 36,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 12),
+            Listener(
+              onPointerDown: (details) {
+                _buttonPressed = true;
+                _increaseCounterWhilePressed();
+              },
+              onPointerUp: (details) {
+                _buttonPressed = false;
+              },
+              child: SizedBox(
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_forward,
+                        size: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('라인트레이싱 컨트롤러'),
+        title: Text('라인트레이싱 컨트롤러'),
       ),
-      body: Container(
-        child: Column(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          physics:
+              BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           children: [
-            InkWell(
-              child: Container(
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _activeAutopilot ? '자동주행 켜짐' : '자동주행 꺼짐',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        SizedBox(height: 4),
-                        Text(_activeAutopilot ? '자동 주행 중입니다.' : '수동주행 모드입니다.')
-                      ],
-                    ),
-                    Expanded(child: Container()),
-                    Switch(
-                      value: _activeAutopilot,
-                      onChanged: (value) {
-                        setState(() {
-                          _activeAutopilot = value;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              onTap: () {
-                setState(() {
-                  _activeAutopilot = !_activeAutopilot;
-                });
-              },
-            ),
-            Divider(height: 0),
-            SizedBox(height: 25),
             Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Listener(
-                      onPointerDown: (details) {
-                        _buttonPressed = true;
-                        _increaseCounterWhilePressed();
-                      },
-                      onPointerUp: (details) {
-                        _buttonPressed = false;
-                      },
-                      child: SizedBox(
-                        height: 70,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              RotationTransition(
-                                turns: AlwaysStoppedAnimation(90 / 360),
-                                child: Icon(
-                                  Icons.arrow_back,
-                                  size: 36,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 12),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Listener(
-                      onPointerDown: (details) {
-                        _buttonPressed = true;
-                        _increaseCounterWhilePressed();
-                      },
-                      onPointerUp: (details) {
-                        _buttonPressed = false;
-                      },
-                      child: SizedBox(
-                        height: 70,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.arrow_back,
-                                size: 36,
+                InkWell(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _activeAutopilot ? '자동주행 켜짐' : '자동주행 꺼짐',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: _activeAutopilot ? Colors.indigo : null,
+                                fontWeight:
+                                    _activeAutopilot ? FontWeight.bold : null,
                               ),
-                            ],
-                          ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(_activeAutopilot
+                                ? '자동 주행 중입니다.'
+                                : '수동주행 모드입니다.')
+                          ],
                         ),
-                      ),
-                    ),
-                    SizedBox(width: 12),
-                    Listener(
-                      onPointerDown: (details) {
-                        _buttonPressed = true;
-                        _increaseCounterWhilePressed();
-                      },
-                      onPointerUp: (details) {
-                        _buttonPressed = false;
-                      },
-                      child: SizedBox(
-                        height: 70,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              RotationTransition(
-                                turns: AlwaysStoppedAnimation(90 / 360),
-                                child: Icon(
-                                  Icons.arrow_forward,
-                                  size: 36,
-                                ),
-                              )
-                            ],
-                          ),
+                        Expanded(child: Container()),
+                        Switch(
+                          value: _activeAutopilot,
+                          onChanged: (value) {
+                            setState(() {
+                              _activeAutopilot = value;
+                            });
+                          },
                         ),
-                      ),
+                      ],
                     ),
-                    SizedBox(width: 12),
-                    Listener(
-                      onPointerDown: (details) {
-                        _buttonPressed = true;
-                        _increaseCounterWhilePressed();
-                      },
-                      onPointerUp: (details) {
-                        _buttonPressed = false;
-                      },
-                      child: SizedBox(
-                        height: 70,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.arrow_forward,
-                                size: 36,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _activeAutopilot = !_activeAutopilot;
+                    });
+                  },
                 ),
+                Divider(height: 0),
+                (_activeAutopilot
+                    ? autoModeControlWidget
+                    : manualModeControlWidget)(context),
               ],
-            )
+            ),
+            HardwarePage(),
           ],
         ),
       ),
-      drawer: MainDrawer(parentContext: context),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.screen_rotation),
+        onPressed: () {
+          if (isRotated) {
+            SystemChrome.setPreferredOrientations(
+              [DeviceOrientation.portraitUp],
+            );
+          } else {
+            SystemChrome.setPreferredOrientations([
+              DeviceOrientation.landscapeLeft,
+              DeviceOrientation.landscapeRight
+            ]);
+          }
+          isRotated = !isRotated;
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeOutExpo,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '메인',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.memory),
+            label: '연결',
+          )
+        ],
+      ),
     );
   }
 }
--- lib/pages/hardware.dart
+++ lib/pages/hardware.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:linetrace_robot_app/widgets/drawer.dart';
 
 class HardwarePage extends StatefulWidget {
   const HardwarePage({Key? key}) : super(key: key);
@@ -24,82 +23,75 @@ class _HardwarePageState extends State<HardwarePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('하드웨어 스캔'),
-        centerTitle: true,
-      ),
-      body: StreamBuilder<List<ScanResult>>(
-          stream: flutterBlue.scanResults,
-          initialData: const [],
-          builder:
-              (BuildContext context, AsyncSnapshot<List<ScanResult>> snapshot) {
-            return RefreshIndicator(
-                child: SizedBox(
-                  height: double.infinity,
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(
-                        parent: AlwaysScrollableScrollPhysics()),
-                    child: Column(
-                      children: snapshot.data!
-                          .where((e) => e.device.name.isNotEmpty)
-                          .map<Widget>((e) {
-                        return SizedBox(
-                          width: double.infinity,
-                          child: Card(
-                              child: ListTile(
-                                onTap: () async {
-                                  await e.device.disconnect();
-                                  await e.device.connect();
+    return StreamBuilder<List<ScanResult>>(
+        stream: flutterBlue.scanResults,
+        initialData: const [],
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ScanResult>> snapshot) {
+          return RefreshIndicator(
+              child: SizedBox(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics()),
+                  child: Column(
+                    children: snapshot.data!
+                            .where((e) => e.device.name.isNotEmpty)
+                            .map<Widget>((e) {
+                          return SizedBox(
+                            width: double.infinity,
+                            child: Card(
+                                child: ListTile(
+                              onTap: () async {
+                                await e.device.disconnect();
+                                await e.device.connect();
 
-                                  final services =
-                                  await e.device.discoverServices();
+                                final services =
+                                    await e.device.discoverServices();
 
-                                  for (var service in services) {
-                                    if (service.uuid.toString().toLowerCase() !=
-                                        '0000FFE0-0000-1000-8000-00805F9B34FB'
-                                            .toLowerCase()) {
-                                      continue;
-                                    }
-                                    var characteristics =
-                                        service.characteristics;
-                                    for (BluetoothCharacteristic c
-                                    in characteristics) {
-                                      await c.write(utf8.encode('CONNECTED\n'));
-                                    }
+                                for (var service in services) {
+                                  if (service.uuid.toString().toLowerCase() !=
+                                      '0000FFE0-0000-1000-8000-00805F9B34FB'
+                                          .toLowerCase()) {
+                                    continue;
                                   }
-                                },
-                                leading: const Icon(Icons.devices),
-                                title: Text(e.device.name),
-                              )),
-                        );
-                      }).toList() +
-                          [
-                            SizedBox(
-                              width: double.infinity,
-                              child: Card(
-                                  child: ListTile(
-                                    onTap: () async {
-                                      final devices =
-                                      await flutterBlue.connectedDevices;
-                                      for (var e in devices) {
-                                        e.disconnect();
-                                      }
-                                    },
-                                    leading: const Icon(Icons.delete),
-                                    title: const Text('연결 해제'),
-                                  )),
-                            ),
-                          ],
-                    ),
+                                  var characteristics = service.characteristics;
+                                  for (BluetoothCharacteristic c
+                                      in characteristics) {
+                                    await c.write(utf8.encode('CONNECTED\n'));
+                                  }
+                                }
+                              },
+                              leading: const Icon(Icons.devices),
+                              title: Text(e.device.name),
+                            )),
+                          );
+                        }).toList() +
+                        [
+                          SizedBox(
+                            width: double.infinity,
+                            child: Card(
+                                child: ListTile(
+                              onTap: () async {
+                                final devices =
+                                    await flutterBlue.connectedDevices;
+                                for (var e in devices) {
+                                  e.disconnect();
+                                }
+                              },
+                              leading: const Icon(Icons.delete),
+                              title: const Text('연결 해제'),
+                            )),
+                          ),
+                        ],
                   ),
                 ),
-                onRefresh: () async {
-                  if (await flutterBlue.isScanning.first) return;
-                  await flutterBlue.startScan(timeout: const Duration(seconds: 4));
-                });
-          }),
-      drawer: MainDrawer(parentContext: context),
-    );
+              ),
+              onRefresh: () async {
+                if (await flutterBlue.isScanning.first) return;
+                await flutterBlue.startScan(
+                    timeout: const Duration(seconds: 4));
+              });
+        });
   }
 }
